f627fba985141904dc4c5fd82ccc59b29dcc8eef
avardaan <oyevardaan@gmail.com>
2023-08-10 03:26:15 -0700

    attempt to parse call transcript and get structured data using langchain

--- patient_intake_agent/constants.py
+++ patient_intake_agent/constants.py
@@ -1,6 +1,37 @@
-INITIAL_MESSAGE = "Hello, I am Vardaan, your intake agent. \
-  To help you get the care you need, I will be asking you a series of questions to process your registration. \
-    Once you are registered, I will help you schedule an appointment with a doctor. To proceed, please confirm."
+from pydantic import BaseModel
+from typing import Optional
 
-PROMPT_PREAMBLE = "The AI is a patient intake agent for a healthcare provider. It will help the patient register and schedule an appointment with a doctor.\
-  For now, just record the patient's first name. After getting the first name, end the conversation."
+
+class _Insurance(BaseModel):
+    payer_name: Optional[str]
+    member_id: Optional[str]
+
+
+class _Referral(BaseModel):
+    has_referral: Optional[bool]
+    to_provider: Optional[str]
+
+
+class PatientData(BaseModel):
+    first_name: Optional[str]
+    last_name: Optional[str]
+    insurance: Optional[_Insurance]
+    referral: Optional[_Referral]
+    chief_complaint: Optional[str]
+
+
+INITIAL_MESSAGE = "Hello! I am Vardaan, your intake agent. \
+  To help you get the care you need, I will be asking you a series of questions to process your registration. \
+    Once you are registered, I will help you schedule an appointment with a doctor. To begin, please say ok."
+
+AGENT_GOAL_DESCRIPTION = (
+    "Retrieve patient information and schedule an appointment with a doctor."
+)
+
+PROMPT_PREAMBLE = f"""
+        The AI is a friendly phone bot built for information intake via inbound calls from patients.
+
+Here is the context for the call:
+Intended goal: {AGENT_GOAL_DESCRIPTION}
+Information to be collected: {PatientData().dict()}
+        """
--- patient_intake_agent/events_manager.py
+++ patient_intake_agent/events_manager.py
@@ -1,12 +1,13 @@
+import typing
 from patient_intake_agent.logger import logger
 from vocode.streaming.utils import events_manager
-from vocode.streaming.models.events import (
-    Event,
-    EventType,
-)
+from vocode.streaming.models.events import Event, EventType
+from vocode.streaming.models.transcript import TranscriptCompleteEvent
+
+from patient_intake_agent.transcript_parser import get_patient_data_from_transcript
 
 
-class CustomEventsManager(events_manager.EventsManager):
+class InboundCallEventsManager(events_manager.EventsManager):
     def __init__(self):
         super().__init__(
             subscriptions=[
@@ -16,7 +17,15 @@ class CustomEventsManager(events_manager.EventsManager):
         )
 
     def handle_event(self, event: Event):
-        logger.info(event)
+        if event.type == EventType.TRANSCRIPT_COMPLETE:
+            transcript_complete_event = typing.cast(TranscriptCompleteEvent, event)
+            # cid = transcript_complete_event.conversation_id
+            parsed_patient_data = get_patient_data_from_transcript(
+                transcript_complete_event.transcript.to_string(include_timestamps=False)
+            )
+            # logger.info(
+            #     f"Patient data parsed from transcript: {parsed_patient_data.json()}"
+            # )
 
 
-custom_events_manager = CustomEventsManager()
+custom_events_manager = InboundCallEventsManager()
